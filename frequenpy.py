badebec52ba91e302a575eb4caffbcd3584a99ab
Tomás Juan Link <tomaslink@gmail.com>
2019-01-26 06:28:26 +0000

    Refactor the entry point

--- frequenpy.py
+++ frequenpy.py
@@ -5,17 +5,22 @@ import traceback
 from frequenpy.beaded_string.beaded_string_factory import BeadedStringFactory
 from frequenpy.beaded_string.animation import Animation
 
+
 NUMBER_OF_FRAMES = 2000
 DEFAULT_SPEED = 1.5
 DEFAULT_SAVE = False
 
+
+DESCRIPTION = 'Welcome to FrequenPy!'
+EPILOG = "Enjoy!"
 HELP = 'Choose a system to simulate'
-BS_HELP = 'Transverse standing wave on a beaded string'
-BS_N_HELP = 'number of masses'
-BS_M_HELP = 'normal modes to combine'
-BS_B_HELP = '0, 1, or 2, meanining fixed, free or mixed ends'
-BS_R_HELP = 'save the animation in mp4 format'
-BS_S_HELP = 'animation speed'
+BEADED_STRING_PARSER_NAME = 'bs'
+BEADED_STRING_HELP = 'Transverse standing wave on a beaded string'
+BEADED_STRING_MASSES_HELP = 'number of masses'
+BEADED_STRING_MODES_HELP = 'normal modes to combine. Ex: 1 2 3'
+BEADED_STRING_BOUNDARY_HELP = '0, 1, or 2, meanining fixed, free or mixed ends'
+BEADED_STRING_SAVE_HELP = 'save the animation in mp4 format'
+BEADED_STRING_SPEED_HELP = 'animation speed. Can be a float number'
 
 
 def execute_bs(boundary, masses, modes, speed, save_animation):
@@ -33,24 +38,65 @@ def execute_bs(boundary, masses, modes, speed, save_animation):
         logging.error(tpl.format(type(e), e))
 
 
-def main():
-    parser = argparse.ArgumentParser()
+def add_beaded_string_parser(subparsers):
+    bs = subparsers.add_parser(
+        BEADED_STRING_PARSER_NAME,
+        help=BEADED_STRING_HELP
+    )
+    req = bs.add_argument_group("Required arguments")
+    req.add_argument(
+        '-n',
+        dest='masses',
+        required=True,
+        help=BEADED_STRING_MASSES_HELP
+    )
+    req.add_argument(
+        '-m',
+        dest='modes',
+        required=True,
+        nargs='+',
+        help=BEADED_STRING_MODES_HELP
+    )
+    req.add_argument(
+        '-b',
+        dest='boundary',
+        required=True,
+        help=BEADED_STRING_BOUNDARY_HELP
+    )
+    req.add_argument(
+        '-s',
+        dest='speed',
+        required=False,
+        help=BEADED_STRING_SPEED_HELP
+    )
+    req.add_argument(
+        '--save',
+        action='store_true',
+        help=BEADED_STRING_SAVE_HELP
+    )
+    return bs
 
-    subparsers = parser.add_subparsers(help=HELP, dest='system')
 
-    bs = subparsers.add_parser('bs', help=BS_HELP)
-    bs.add_argument('-n', '--masses', required=True, help=BS_N_HELP)
-    bs.add_argument('-m', '--modes', required=True, nargs='+', help=BS_M_HELP)
-    bs.add_argument('-b', '--boundary', required=True, help=BS_B_HELP)
-    bs.add_argument('-s', '--speed', required=False, help=BS_S_HELP)
-    bs.add_argument('--save', action='store_true', help=BS_R_HELP)
-
+def validate_args_length(parser, subparsers):
     if len(sys.argv) < 2:
-        parser.print_help()
+            parser.print_help()
+            sys.exit(1)
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == BEADED_STRING_PARSER_NAME:
+            subparsers.choices[BEADED_STRING_PARSER_NAME].print_help()
         sys.exit(1)
 
+
+def main():
+
+    parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG)
+    subparsers = parser.add_subparsers(help=HELP, dest='system')
+    add_beaded_string_parser(subparsers)
+    validate_args_length(parser, subparsers)
+
     args = parser.parse_args()
-    if args.system == 'bs':
+
+    if args.system == BEADED_STRING_PARSER_NAME:
         execute_bs(
             int(args.boundary),
             int(args.masses),
