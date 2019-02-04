7e2c6f07f464bd3e389507a10969b0cc89f91c7a
Tomás Juan Link <tomaslink@gmail.com>
2019-02-04 02:07:05 +0000

    Remove the matrix eigenvalues approach to get frequencies
    * Use the dispersion relation to obtain frequencies
    * Fix the free ends motion

--- frequenpy/beaded_string/beaded_string_factory.py
+++ frequenpy/beaded_string/beaded_string_factory.py
@@ -6,48 +6,15 @@ BOUNDARY_FIXED = 0
 BOUNDARY_MIXED = 1
 BOUNDARY_FREE = 2
 
-LONGITUDE = 1.5
-AMPLITUDE = 0.4
 
-
-class BeadedStringFactory(object):
-
-    @staticmethod
-    def validate_parameters(N, modes):
-
-        if N < 1 or N > 40:
-            raise ValueError(
-                "Number of masses must be an integer number between 1 and 40")
-
-        if len(modes) > N:
-            raise ValueError(
-                ("The number of normal modes must be less "
-                    "or equal than the number of masses!"))
-
-        for mode in modes:
-            if mode > N:
-                raise ValueError(
-                    "The max. normal mode for this system is {}".format(N))
-            if mode < 1:
-                raise ValueError("The min. normal mode is 1!")
-
-    @staticmethod
-    def create(boundary_condition, number_of_masses, normal_modes):
-        BeadedStringFactory.validate_parameters(number_of_masses, normal_modes)
-        if boundary_condition == BOUNDARY_FIXED:
-            return BeadedStringFixed(
-                number_of_masses, normal_modes, LONGITUDE, AMPLITUDE
-            )
-        if boundary_condition == BOUNDARY_MIXED:
-            return BeadedStringMixed(
-                number_of_masses, normal_modes, LONGITUDE, AMPLITUDE
-            )
-        if boundary_condition == BOUNDARY_FREE:
-            return BeadedStringFree(
-                number_of_masses, normal_modes, LONGITUDE, AMPLITUDE
-            )
-        else:
-            not_valid_msg = "{} is not a valid boundary_condition"
-            raise NotImplementedError(
-                not_valid_msg.format(boundary_condition)
-            )
+def create(number_of_beads, modes, boundary):
+    if boundary == BOUNDARY_FIXED:
+        return BeadedStringFixed(number_of_beads, modes)
+    if boundary == BOUNDARY_MIXED:
+        return BeadedStringMixed(number_of_beads, modes)
+    if boundary == BOUNDARY_FREE:
+        return BeadedStringFree(number_of_beads, modes)
+    else:
+        raise NotImplementedError(
+            "{} is not a valid boundary condition".format(boundary)
+        )
